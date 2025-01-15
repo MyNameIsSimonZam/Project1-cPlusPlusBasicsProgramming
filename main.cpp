401ebc71dcae7b211ddf91558ced1087d87b19c8
Simon <semazamesin@gmail.com>
2025-01-15 18:19:53 +0500

    add task3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 ﻿#include <iostream>
+
 void task1();
 void task2();
+void task3();
+
 
 int main() {
     int choice;
     std::cout << "Choose a task to run:\n";
     std::cout << "1. Task 1\n";
     std::cout << "2. Task 2\n";
+    std::cout << "2. Task 3\n";
     std::cout << "Enter your choice: ";
     std::cin >> choice;
 
@@ -16,6 +20,9 @@ int main() {
         break;
     case 2:
         task2();
+        break;
+    case 3:
+        task3();
         break;
     default:
         std::cout << "Invalid choice. Exiting...\n";
